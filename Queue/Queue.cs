af5273d0dc8379322583d7a8aa4fcfeb50be3116
дмитрий музыка <dima.muzyka.1914@mail.ru>
2022-02-21 10:28:15 +0300

    change

---
+++ Queue/Queue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Queue
+    {
+        int[] arr;
+        int current;
+        int currentIn;
+        int currentOut;
+        int count;
+        int val;
+        public void Queue(int count)
+        {
+            this.count = count;
+        }
+        void Push(int val)
+        {
+
+        }
+    }
+}
